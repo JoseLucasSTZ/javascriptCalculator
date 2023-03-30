33a0d46e7dce369cadeac1524b4148fc7bc2323c
José Lucas Steinmetz da Silveira <65045856+joselucasSteinmetz@users.noreply.github.com>
2023-03-29 21:09:23 -0300

    Update
    Update. I  still have a lot of work to do, but soon I'll finisht it!

---
+++ script.js
@@ -0,0 +1,20 @@
+let n1
+let n2
+let ns = []
+let operation
+let variables = []
+
+function getValue(valor) {
+    ns.push(valor)
+    alert(ns)
+}
+
+function getOperation(valor) {
+    operation.push(valor)
+    alert(operation)
+}
+
+function getVariable(valor) {
+    variables.push(valor)
+    alert(variables)
+}
